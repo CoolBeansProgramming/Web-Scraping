7cb785a594606f169335ad98eb46011206bc797a
CoolBeansProgramming <50372589+CoolBeansProgramming@users.noreply.github.com>
2023-02-05 15:26:42 -0500

    init

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 
 # get URL
-URL = "https://realpython.github.io/fake-jobs/"
 
-page = requests.get(URL)
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=java&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
-print(page.text)
+    for index, job in enumerate(jobs):
+        added_date = job.find('span', class_='sim-posted').span.text
+    
+        if 'few' in added_date:
+            company_name = job.find('h3', class_='joblist-comp-name').text.upper()
+            # experience = job.find('li', class_= False)
+            experience = job.find('ul', class_='top-jd-dtl clearfix').text.strip()[11:]
+            skills = job.find('span', class_= 'srp-skills').text.replace(' ','')
+            info = job.header.h2.a['href']
+            
+            
+            with open(f'posts/{index}.txt','w') as f:
+                f.write(f'Company: {company_name.strip()}\n')
+                f.write(f'Experience: {experience}\n')
+                f.write(f'Skills: {skills.strip()}\n')
+                f.write(f'Info: {info}')
+            print(f'File saved: {index}')
+            
+if __name__ == '__main__':
+    find_jobs()
