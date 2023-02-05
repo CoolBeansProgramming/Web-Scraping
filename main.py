bf1be96f23de6b73cdfb47f239e51ac8acb5a504
CoolBeansProgramming <50372589+CoolBeansProgramming@users.noreply.github.com>
2023-02-05 16:58:34 -0500

    added excel script and worksheet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-# get URL
 
 def find_jobs():
     html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=java&txtLocation=').text
@@ -11,11 +10,11 @@ def find_jobs():
     for index, job in enumerate(jobs):
         added_date = job.find('span', class_='sim-posted').span.text
     
-        if 'few' in added_date:
+        if ('few' in added_date) or ('1' in added_date):
             company_name = job.find('h3', class_='joblist-comp-name').text.upper()
-            # experience = job.find('li', class_= False)
-            experience = job.find('ul', class_='top-jd-dtl clearfix').text.strip()[11:]
+            experience = job.find('ul', class_='top-jd-dtl clearfix').text.strip()[11:20]
             skills = job.find('span', class_= 'srp-skills').text.replace(' ','')
+            location = job.find('ul', class_='top-jd-dtl clearfix').text.strip()[34:]
             info = job.header.h2.a['href']
             
             
@@ -23,8 +22,10 @@ def find_jobs():
                 f.write(f'Company: {company_name.strip()}\n')
                 f.write(f'Experience: {experience}\n')
                 f.write(f'Skills: {skills.strip()}\n')
+                f.write(f'Location: {location}\n')
                 f.write(f'Info: {info}')
             print(f'File saved: {index}')
+        
             
 if __name__ == '__main__':
     find_jobs()
